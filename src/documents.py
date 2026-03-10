91f56a49b9178853d33dbbbf01908d8853edaf12
ClemDetr <clemence.detroyat@gmail.com>
2026-03-10 10:09:14 +0100

    Create documents.py

---
+++ src/documents.py
@@ -0,0 +1,7 @@
+"""Documents"""
+from texte import Texte
+
+class Roman(Texte):
+    """class roman apparenté à texte"""
+    def __init__(self, ):
+        super().__init__(titre, auteur, contenu, annee)
